a34288491fed05ffa4d7cdfa064ab93b18c9274c
Julia Guenther <juliag1@alumni.stanford.edu>
2015-10-11 21:01:40 -0700

    returns numerator and denominator

---
+++ get-num-den.py
@@ -0,0 +1,10 @@
+class Fraction:
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
+
+    def getNum(self):
+        return self.num
+
+    def getDen(self):
+        return self.den
